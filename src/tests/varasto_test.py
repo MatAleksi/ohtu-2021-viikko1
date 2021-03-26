0d447b124984aeb9ce1158aec15035bb2a482613
MatAleksi <mattila.aleksi@hotmail.com>
2021-03-26 17:39:08 +0200

    tehtävä 8

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,40 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_varastoon_ei_voi_laittaa_liikaa(self):
+        self.varasto.lisaa_varastoon(12)
+
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_varastosta_ei_saa_liikaa(self):
+        self.varasto.lisaa_varastoon(5)
+
+        saatu_maara = self.varasto.ota_varastosta(7)
+
+        self.assertAlmostEqual(saatu_maara, 5)
+
+    def test_varasto_ei_saa_negatiivista_tilavuutta(self):
+        varasto = Varasto(-1)
+        
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_varaston_alku_saldo_ei_voi_olla_negatiivinen(self):
+        varasto = Varasto(10, -1)
+
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_varastoon_ei_lisata_negatiivista(self):
+        self.varasto.lisaa_varastoon(-1)
+
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_varstosta_ei_saa_negatiivista(self):
+        self.varasto.lisaa_varastoon(1)
+
+        saatu_maara = self.varasto.ota_varastosta(-1)
+
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_str_palauttaa_oikein(self):
+        self.assertAlmostEqual(self.varasto.__str__(), "saldo = 0, vielä tilaa 10")
